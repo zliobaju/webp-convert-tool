994e3872f76b55f457730d62d0feff00ade5783e
Julius Zlioba <julius.zlioba@connect724.at>
2020-08-06 08:42:37 +0200

    pretified code

--- README.md
+++ README.md
@@ -3,3 +3,7 @@ Tool to convert all JPG and PNG automatically
 
 ## Requirements
 needs 'sourceFolder' in root directory
+
+## Usage
+just run "RUN.cmd" for quick auto convert
+... or use "npm run convert"
--- convert-app.js
+++ convert-app.js
@@ -1,52 +1,52 @@
-const chalk = require('chalk');
-const imagemin = require('imagemin');
-const imageminWebp = require('imagemin-webp');
-const imageminPngquant = require('imagemin-pngquant');
-const imageminMozjpeg = require('imagemin-mozjpeg');
+const chalk = require("chalk");
+const imagemin = require("imagemin");
+const imageminWebp = require("imagemin-webp");
+const imageminPngquant = require("imagemin-pngquant");
+const imageminMozjpeg = require("imagemin-mozjpeg");
 
-let PNGImages = "./sourceFolder/*.png";         // PNG images
-let JPEGImages = "./sourceFolder/*.jpg";        // JPEG images
+let PNGImages = "./sourceFolder/*.png"; // PNG images
+let JPEGImages = "./sourceFolder/*.jpg"; // JPEG images
 
 imagemin([PNGImages], {
-  destination: __dirname + '/exportFolder/',
+  destination: __dirname + "/exportFolder/",
   plugins: [
     imageminWebp({
-      lossless: true
-    })
-  ]
+      lossless: true,
+    }),
+  ],
 }).then(() => {
-  console.log(chalk.green('PNG > WEBP optimized'));
+  console.log(chalk.green("PNG > WEBP optimized"));
 });
 
 imagemin([PNGImages], {
-  destination: __dirname + '/exportFolder/',
+  destination: __dirname + "/exportFolder/",
   plugins: [
     imageminPngquant({
-      quality: [0.6, 0.8]
-    })
-  ]
+      quality: [0.6, 0.8],
+    }),
+  ],
 }).then(() => {
-  console.log(chalk.green('PNG > PNG optimized'));
+  console.log(chalk.green("PNG > PNG optimized"));
 });
 
 imagemin([JPEGImages], {
-  destination: __dirname + '/exportFolder/',
+  destination: __dirname + "/exportFolder/",
   plugins: [
     imageminWebp({
-      quality: 70
-    })
-  ]
+      quality: 70,
+    }),
+  ],
 }).then(() => {
-  console.log(chalk.green('JPEG > WEBP optimized'));
+  console.log(chalk.green("JPEG > WEBP optimized"));
 });
 
 imagemin([JPEGImages], {
-  destination: __dirname + '/exportFolder/',
+  destination: __dirname + "/exportFolder/",
   plugins: [
     imageminMozjpeg({
-      quality: 80
-    })
-  ]
+      quality: 80,
+    }),
+  ],
 }).then(() => {
-  console.log(chalk.green('JPEG > JPEG optimized'));
+  console.log(chalk.green("JPEG > JPEG optimized"));
 });
