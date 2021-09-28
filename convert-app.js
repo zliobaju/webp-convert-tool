64f93fae364d82aefad0c4fdd835fab173d54843
Julius Zlioba <julius.zlioba@connect724.at>
2021-09-28 08:03:12 +0200

    updated imagemin

--- convert-app.js
+++ convert-app.js
@@ -1,14 +1,14 @@
-const chalk = require("chalk");
-const imagemin = require("imagemin");
-const imageminWebp = require("imagemin-webp");
-const imageminPngquant = require("imagemin-pngquant");
-const imageminMozjpeg = require("imagemin-mozjpeg");
+import imagemin from 'imagemin';
+import chalk from 'chalk'
+import imageminPngquant from 'imagemin-pngquant';
+import imageminWebp from "imagemin-webp";
+import imageminMozjpeg from 'imagemin-mozjpeg'
 
 let PNGImages = "./sourceFolder/*.png"; // PNG images
 let JPEGImages = "./sourceFolder/*.jpg"; // JPEG images
 
-imagemin([PNGImages], {
-  destination: __dirname + "/exportFolder/",
+const pngToWebp = await imagemin([PNGImages], {
+  destination: 'exportFolder',
   plugins: [
     imageminWebp({
       lossless: true,
@@ -18,19 +18,19 @@ imagemin([PNGImages], {
   console.log(chalk.green("PNG > WEBP optimized"));
 });
 
-imagemin([PNGImages], {
-  destination: __dirname + "/exportFolder/",
-  plugins: [
-    imageminPngquant({
-      quality: [0.6, 0.8],
-    }),
-  ],
+const pngToPngFiles = await imagemin([PNGImages], {
+	destination: 'exportFolder',
+	plugins: [
+		imageminPngquant({
+			quality: [0.6, 0.8]
+		})
+	]
 }).then(() => {
   console.log(chalk.green("PNG > PNG optimized"));
 });
 
-imagemin([JPEGImages], {
-  destination: __dirname + "/exportFolder/",
+const jpegToWebpFiles = await imagemin([JPEGImages], {
+  destination: "exportFolder",
   plugins: [
     imageminWebp({
       quality: 70,
@@ -40,8 +40,8 @@ imagemin([JPEGImages], {
   console.log(chalk.green("JPEG > WEBP optimized"));
 });
 
-imagemin([JPEGImages], {
-  destination: __dirname + "/exportFolder/",
+const jpegToJepgFiles = await imagemin([JPEGImages], {
+  destination: "exportFolder",
   plugins: [
     imageminMozjpeg({
       quality: 80,
